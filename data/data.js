b412743adc396461feafae9c5277e0a01cb665df
homer-wh <homer2coder@gmail.com>
2017-02-13 20:40:08 +0800

    test by safari & change images

--- data/data.js
+++ data/data.js
@@ -1,82 +1,65 @@
-var data = [
-  {
-    "fileName": "1.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer."
-  },
-  {
-    "fileName": "2.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer."
-  },
-  {
-    "fileName": "3.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer."
-  },
-  {
-    "fileName": "4.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer. "
-  },
-  {
-    "fileName": "5.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer. "
-  },
-  {
-    "fileName": "6.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer. "
-  },
-  {
-    "fileName": "7.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer. "
-  },
-  {
-    "fileName": "8.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer. "
-  },
-  {
-    "fileName": "9.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer. "
-  },
-  {
-    "fileName": "10.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer. "
-  },
-  {
-    "fileName": "11.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer. "
-  },
-  {
-    "fileName": "12.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer.  "
-  },
-  {
-    "fileName": "13.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer.  "
-  },
-  {
-    "fileName": "14.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer.  "
-  },
-  {
-    "fileName": "15.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer.  "
-  },
-  {
-    "fileName": "16.jpg",
-    "title": "Heaven of time",
-    "desc": "Here he comes Here comes Speed Racer.  "
-  }
-]
+var data = [{
+  "fileName": "1.jpg",
+  "title": "This is us",
+  "desc": "你给了我一颗心，我想给你全世界."
+}, {
+  "fileName": "2.jpg",
+  "title": "This is us",
+  "desc": "希望我们的生活像走在平坦的石板路上，大步向前."
+}, {
+  "fileName": "3.jpg",
+  "title": "This is us",
+  "desc": "你向我伸出了手，我愿意牵着陪你大步走下去."
+}, {
+  "fileName": "4.jpg",
+  "title": "This is us",
+  "desc": "如果我们遇到了不如意的时候，别过头，很快就会过去，美好的生活会继续."
+}, {
+  "fileName": "5.jpg",
+  "title": "This is us",
+  "desc": "当我们老了的时候，依然可以坐在摇椅上，细数往日时光."
+}, {
+  "fileName": "6.jpg",
+  "title": "This is us",
+  "desc": "不管什么时候，我都希望你能像个幸福的小姑娘，可以在秋千上荡漾快乐."
+}, {
+  "fileName": "7.jpg",
+  "title": "This is us",
+  "desc": "偶尔，我们也要酷酷地去探索世界."
+}, {
+  "fileName": "8.jpg",
+  "title": "This is us",
+  "desc": "经常，我说的不多，那是因为我不需要说很多."
+}, {
+  "fileName": "9.jpg",
+  "title": "This is us",
+  "desc": "我们有乐观的心态，走到哪里都可以体验生活."
+}, {
+  "fileName": "10.jpg",
+  "title": "This is us",
+  "desc": "我们的笑容，像画一样，镌刻在时间里."
+}, {
+  "fileName": "11.jpg",
+  "title": "This is us",
+  "desc": "有时候，我们换一个角度看世界，又可以玩得很快乐."
+}, {
+  "fileName": "12.jpg",
+  "title": "This is us",
+  "desc": "除了诗，我们还有远方，迎着阳光，出发."
+}, {
+  "fileName": "13.jpg",
+  "title": "This is us",
+  "desc": "背对速度与激情，生活还有宁静的美好."
+}, {
+  "fileName": "14.jpg",
+  "title": "This is us",
+  "desc": "当你有烦恼的时候，请把它们像泡泡一样吹到阳光下."
+}, {
+  "fileName": "15.jpg",
+  "title": "This is us",
+  "desc": "光着脚，也要跳到高处，让裙子和头发随风飘扬."
+}, {
+  "fileName": "16.jpg",
+  "title": "This is us",
+  "desc": "当樱花盛开的时候，我要牵着你的手穿过树下，一不小心就白了头."
+}]
